168d7d4a5fabc42134869b4353634a66131c195c
Fernando.Helio <febolivarbr@est.utn.ac.cr>
2024-09-18 16:31:52 -0600

    Se agrego el controlador
    Ayuda

---
+++ Controllers/CustomerAppointment.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controllers;
+
+import Models.Appointment;
+import Models.IAppointment;
+import Views.IView;
+
+/**
+ *
+ * @author Fernando
+ */
+public class CustomerAppointment {
+    private IAppointment<Appointment> model;
+    private IView<Appointment> view;
+
+    public CustomerAppointment(IAppointment model, IView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    
+}
