fa352e8103a2b47ff0221220fdcb6cc8b5742cbe
TodV <d2tod12@gmail.com>
2024-09-18 16:19:45 -0600

    Clase Resume y ResumeStack
    Se crearon las clases y se empezó con la clase controlador

--- Models/ResumeStack.java
+++ Models/ResumeStack.java
@@ -4,12 +4,35 @@
  */
 package Models;
 
+import java.util.List;
+import java.util.Stack;
+
 
 /**
  *
  * @author d2tod
  */
-public class ResumeStack {
+public class ResumeStack implements IResume<Resume> {
+    private Stack<Resume> resumeStack; 
+
+    public ResumeStack(){
+        resumeStack = new Stack<>();
+    }
+    
+    @Override
+    public boolean add(Resume element) {
+        return resumeStack.add(element);
+    }
+
+    @Override
+    public Resume get(Resume element) {
+        return resumeStack.peek();
+    }
+
+    @Override
+    public List getAll() {
+        return resumeStack;
+    }
     
     
 }
